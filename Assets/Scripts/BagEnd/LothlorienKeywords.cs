4c7240b3b75a807e1f28d093bb29fe5c88c799d9
paul@insidedown.com <paul@insidedown.com>
2018-11-09 20:59:24 -0500

    - adding drawing abilities

---
+++ Assets/Scripts/BagEnd/LothlorienKeywords.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.Windows.Speech;
+using System;
+
+public class LothlorienKeywords : MonoBehaviour {
+
+    public GameObject Person1DrawingHolderLeft;
+    public GameObject Person1DrawingHolderRight;
+    public GameObject Person1Prefab;
+
+    public GameObject Person2DrawingHolderLeft;
+    public GameObject Person2DrawingHolderRight;
+    public GameObject Person2Prefab;
+
+    private Dictionary<string, Action> keywordActions = new Dictionary<string, Action>();
+    private KeywordRecognizer keywordRecognizer;
+
+	// Use this for initialization
+	void Start () {
+        keywordActions.Add("start drawing", StartDrawing);
+        keywordActions.Add("erase", Erase);
+
+        keywordRecognizer = new KeywordRecognizer(keywordActions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += OnKeywordsRecognized;
+        keywordRecognizer.Start();
+	}
+
+    void OnKeywordsRecognized(PhraseRecognizedEventArgs args)
+    {
+        Debug.Log("Keyword: " + args.text);
+        keywordActions[args.text].Invoke();
+    }
+
+
+    void StartDrawing()
+    {
+        Debug.Log("start drawing called");
+        Instantiate(Person1Prefab, Person1DrawingHolderLeft.transform);
+        Instantiate(Person1Prefab, Person1DrawingHolderRight.transform);
+    }
+
+    void DestroyChildren(GameObject parentGameObj)
+    {
+        if (parentGameObj == null)
+            return; 
+
+        foreach(Transform child in parentGameObj.transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+    void Erase()
+    {
+        Debug.Log("erase");
+        DestroyChildren(Person1DrawingHolderLeft);
+        DestroyChildren(Person1DrawingHolderRight);
+    }
+	
+}
